96064e937e63c3f92be5e61000b18d5558105bf1
samiul2102013 <samiulhasan0186@gmail.com>
2026-05-20 02:06:38 -0700

    docerf

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -4,16 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    re_path(r'^config/?$', views.AppConfigView.as_view()),
     re_path(r'^content/help-support/?$', views.HelpSupportPageView.as_view()),
     re_path(r'^content/privacy-policy/?$', views.PrivacyPolicyPageView.as_view()),
-    
     re_path(r'^admin/content-pages/?$', views.AdminContentPageListCreateView.as_view()),
     re_path(r'^admin/content-pages/(?P<slug>[-\w]+)/?$', views.AdminContentPageDetailView.as_view()),
-    
-    re_path(r'^admin/faqs/?$', views.AdminFAQListCreateView.as_view()),
-    re_path(r'^admin/faqs/(?P<pk>\d+)/?$', views.AdminFAQDetailView.as_view()),
-    
-    re_path(r'^admin/support-contact/?$', views.AdminSupportContactView.as_view()),
-    
-    re_path(r'^config/?$', views.AppConfigView.as_view()),
 ]
