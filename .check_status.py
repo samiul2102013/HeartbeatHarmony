b924079a74a0f27ef2938062eb2a96a9ac4081a3
samiul2102013 <samiulhasan0186@gmail.com>
2026-06-20 04:20:18 -0700

    Clean temp files

--- .check_status.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-host, user, password = '2.24.115.93', 'root', 'HartbeatWellness@Portia123'
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username=user, password=password, timeout=10)
-
-stdin, stdout, stderr = client.exec_command("docker ps -a --filter name=hartbeat --format 'table {{.Names}}\t{{.Status}}'", timeout=10)
-print(stdout.read().decode('utf-8', errors='replace').strip())
-client.close()
