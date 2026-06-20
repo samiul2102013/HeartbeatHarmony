c6e08ae0c8ac7880979e4dd901845f4e703cd664
samiul2102013 <samiulhasan0186@gmail.com>
2026-06-20 03:29:23 -0700

    Simplify URL paths: /privacy, /terms-condition, /delete-account

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,9 +19,9 @@ urlpatterns = [
     path('api/', include('apps.pricing.urls')),
     path('api/', include('apps.core.urls')),
     path('api/', include('apps.iap.urls')),
-    path('privacy-policy/', views.PrivacyPolicyPageView.as_view(), name='privacy-policy'),
-    path('terms-and-conditions/', views.TermsAndConditionsPageView.as_view(), name='terms-and-conditions'),
-    path('delete-account-policy/', views.DeleteAccountPolicyView.as_view(), name='delete-account-policy'),
+    path('privacy/', views.PrivacyPolicyPageView.as_view(), name='privacy-policy'),
+    path('terms-condition/', views.TermsAndConditionsPageView.as_view(), name='terms-and-conditions'),
+    path('delete-account/', views.DeleteAccountPolicyView.as_view(), name='delete-account-policy'),
 
     # API Docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
