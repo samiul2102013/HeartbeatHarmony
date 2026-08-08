e8ddb1f597011da669f8ae583fbb49cca557e9b7
samiul2102013 <samiulhasan0186@gmail.com>
2026-08-08 18:20:53 +0600

    vps.py updatd

--- vps_get_compose.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('2.24.115.93', username='root', password='HartbeatWellness@Portia123')
-
-stdin, stdout, stderr = c.exec_command("cat /etc/dokploy/compose/hartbeatstack-hartbeatstack-buv5uf/code/docker-compose.dokploy.yml")
-print(stdout.read().decode())
-c.close()
--- vps_test_login_api.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('2.24.115.93', username='root', password='HartbeatWellness@Portia123')
-
-stdin, stdout, stderr = c.exec_command("curl -s -X POST http://localhost:8005/api/auth/login/ -H 'Content-Type: application/json' -d '{\"email\":\"support@ICSNCardiology.org\",\"password\":\"Admin@123456\"}' 2>&1")
-print(stdout.read().decode()[:300])
-c.close()
